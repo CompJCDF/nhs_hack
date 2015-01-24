327099282e822561da792bd203598d74b0e590a8
Will Webberley <willwebberley@gmail.com>
2015-01-24 13:44:18 +0000

    dynamic set loading (ish)

--- js/app/app.js
+++ js/app/app.js
@@ -1,6 +1,7 @@
 var ui = {
     controls_opener: null,
-    control_panel: null
+    control_panel: null,
+    data_sets: null
 };
 
 var net = {
@@ -20,6 +21,8 @@ var net = {
     }
 };
 
+var data_sets = [];
+
 function log(text){
     console.log(text);
 }
@@ -27,6 +30,7 @@ function log(text){
 function init_ui(){
     ui.controls_opener = document.getElementById("controls_opener");
     ui.control_panel = document.getElementById("control_panel");
+    ui.data_sets = document.getElementById("data_sets");
     if(! /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
         add_class(ui.control_panel, "open");
     }
@@ -39,7 +43,34 @@ function bind_listeners(){
 function load_metadata(){
     net.get("data/descriptors.json", function(data){
         log(data); 
+        data_sets = [
+            {name:'Set 1', id:1, enabled: false},
+            {name:'Set 2', id:2, enabled: false},
+            {name:'Set 3', id:3, enabled: false},
+            {name:'Set 4', id:4, enabled: false},
+            {name:'Set 5', id:5, enabled: false}
+        ];
+
+        for(var i = 0; i < data_sets.length; i++){
+            var e = "<li id='set_"+data_sets[i].id+"' onclick='toggle_set("+data_sets[i].id+");'>"+data_sets[i].name+"</li>";
+            ui.data_sets.innerHTML += e;
+        }
+        bind_listeners();
     });   
+}
+
+function toggle_set(set_id){
+    var set = null;
+    for(var i = 0; i < data_sets.length; i++){
+        if(data_sets[i].id == set_id){
+            set=data_sets[i];
+            add_class(document.getElementById("set_"+set.id), "ticked");
+        }
+        else{
+            remove_class(document.getElementById("set_"+data_sets[i].id), "ticked");
+        }
+    }
+    set.enabled = !set.enabled;
 }
 
 function toggle_controls(){
@@ -69,3 +100,4 @@ function remove_class(el, name){
 
 init_ui();
 bind_listeners();  
+load_metadata();
