eb89027319e1c0436a6584e2a8ac5be7df669b11
Will Webberley <willwebberley@gmail.com>
2015-01-25 10:48:09 +0000

    @

--- js/app/app.js
+++ js/app/app.js
@@ -5,6 +5,12 @@ var ui = {
     timeline: null,
     timeline_slider: null,
     map: null,
+    config_opener: null,
+    config_overlay: null,
+    store_data: null,
+    config_closer: null,
+    json_field: null,
+    csv_field: null
 };
 
 var net = {
@@ -39,6 +45,12 @@ function init_ui(){
     ui.timeline = document.getElementById("timeline");
     ui.timeline_slider = timeline.getElementsByTagName("div")[0];
     ui.map = document.getElementById("map");
+    ui.config_opener = document.getElementById("add_data");
+    ui.config_overlay = document.getElementById("config_overlay");
+    ui.store_data = document.getElementById("store_data");
+    ui.config_closer = document.getElementById("close_config");
+    ui.json_field = document.getElementById("descriptor");
+    ui.csv_field = document.getElementById("csv_data");
     if(! /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
         toggle_controls(); 
     }
@@ -46,15 +58,75 @@ function init_ui(){
 
 function bind_listeners(){
     ui.controls_opener.onclick = toggle_controls;
+    ui.config_opener.onclick = toggle_config;
+    ui.config_closer.onclick = toggle_config;
+    ui.store_data.onclick = store_data;
+}
+
+function store_data(){
+    var json = ui.json_field.value;
+    var csv = ui.csv_field.value;
+    
+    var id = parseInt(new Date().getTime());
+    var descriptor = JSON.parse(json);
+    descriptor.spath = id;
+    var stored_descriptors = localStorage.local_descriptors;
+    var stored_data = localStorage.local_data;
+
+    try{
+        stored_descriptors = JSON.parse(stored_descriptors);
+    }catch(err){stored_descriptors = [];}
+    try{
+        stored_data = JSON.parse(stored_data);
+    }catch(err){stored_data = [];}
+
+    stored_descriptors.push(descriptor);
+    var data = {};
+    data.id = id;
+    data.csv = csv;
+    stored_data.push(data);
+    localStorage.local_data = JSON.stringify(stored_data);   
+    localStorage.local_descriptors = JSON.stringify(stored_descriptors); 
+    toggle_config();
+    load_metadata();
+}
+
+function get_local_dataset(id){
+    var stored_data = localStorage.local_data;
+    try{
+        stored_data = JSON.parse(stored_data);
+    }catch(err){return null;}
+    
+    for(var i = 0; i < stored_data.length; i++){
+        if(stored_data[i].id == id){
+            return stored_data[i].csv;
+        }
+    }
+}
+
+function get_local_metadata(){
+    var stored_descriptors = localStorage.local_descriptors;
+    try{
+        stored_descriptors = JSON.parse(stored_descriptors);
+        return stored_descriptors;
+    }catch(err){return [];}
 }
 
 function load_metadata(){
-    net.get("data/descriptors.json", function(data){
-        log(data); 
-        if(data.error == true){return;}
-        data_sets = data;
-        for(var i = 0; i < data_sets.length; i++){data_sets[i].id = i+1;}
+    ui.data_sets.innerHTML = "";
+    var local_descriptors = get_local_metadata();    
+        
+    for(var i = 0; i < local_descriptors.length; i++){
+        data_sets.push(local_descriptors[i]);        
+    }
 
+    net.get("data/descriptors.json", function(data){
+        if(data.error == false){
+            for(var i = 0; i < data.length; i++){
+                data_sets.push(data[i]);
+            }
+        }
+        for(var i = 0; i < data_sets.length; i++){data_sets[i].id = i+1;}
 
         for(var i = 0; i < data_sets.length; i++){
             var e = "<li id='set_"+data_sets[i].id+"' >";
@@ -66,6 +138,7 @@ function load_metadata(){
             ui.data_sets.innerHTML += e+"</lI>";
         }
     });  
+    
 }
 
 function load_set(set_id){
@@ -150,6 +223,11 @@ function hide_timeline(){
 function toggle_controls(){
     toggle_class(ui.control_panel, "open");
     toggle_class(ui.controls_opener, "open");
+}
+
+function toggle_config(){
+    toggle_class(ui.config_overlay, "shown");
+    ui.json_field.value = document.getElementById("template").value;
 }
 
 function toggle_class(el, name){
